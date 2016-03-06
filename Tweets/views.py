ef13d8cf4859bde18efa76bdd4a79ffa4fdac6a0
Jeff L <j.lu368@gmail.com>
2016-03-06 13:10:26 -0600

    Changes to location of files and html
    I moved files to where they needed to be and modified the html to work
    with Django.

--- Tweets/views.py
+++ Tweets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, Http404, JsonResponse
 from django.template import RequestContext
+from django.core import serializers
 from django.views.generic.list import ListView
 from .models import Tweet, Region
 from .tweetHelper import analysis
@@ -8,6 +9,7 @@ import requests
 
 class IndexView(ListView):
     template_name = 'Flu/index.html'
+    context_object_name = 'regionList'
 
     def get_queryset(self):
-        return Region.objects.all().order_by("subunit")
+        return Region.objects.all()
